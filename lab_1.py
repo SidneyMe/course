50b66d2985da341270cde8b4493f9c2e019571e1
SidneyMe <147240957+SidneyMe@users.noreply.github.com>
2023-10-18 21:31:35 +0300

    Added lab_2 , sorted labs into different files

---
+++ lab_1.py
@@ -0,0 +1,35 @@
+import hashlib 
+import itertools 
+import string 
+
+def find_string(*args):
+    hash_value, lenght = args
+    characters = string.ascii_letters + string.digits # alphabet creator "AaBbCc.." +  "123.."
+    for attempt in itertools.product(characters, repeat=lenght): # going through all possible strs
+        attempt = ''.join(attempt)
+        # print(attempt) if you want watch a mess )
+        sha3_hash = hashlib.sha3_256(attempt.encode()).hexdigest() # encode attempted str
+        # print(sha3_hash)
+        if sha3_hash == hash_value:
+            return attempt
+    return None
+
+def main():
+    hash_values = [
+        'a03ab19b866fc585b5cb1812a2f63ca861e7e7643ee5d43fd7106b623725fd67',
+        'd182aed568b01fee105557a1d173791c798030db267cf94e17102b94dcbbda3c',
+        '7b6a784b05c64d2e669e026fc61296eca2ee8acd5112eb8ae5f16023809e203b'
+    ]
+
+    for hash_value in hash_values:
+        print(f'Finding string for hash: {hash_value}')
+        for length in range(1, 7): # < correct length here
+            result = find_string (hash_value, length)
+            if result is not None:
+                print(f'Found string: {result}\n')
+                break
+        else :
+            print('No string found\n')
+
+if __name__ == '__main__':
+    main()
---
+++ lab_2.py
@@ -0,0 +1,85 @@
+class BigNumber:  
+    def __init__(self):
+        self.data = []
+
+    def setFromHexString(self, hexString):
+        self.data = [int(hexString[i:i+8], 16) for i in range(0, len(hexString), 8)]
+
+    def getAsHexString(self):
+        return ''.join('{:08x}'.format(i) for i in self.data)
+
+    def INV(self):
+        self.data = [~i & 0xFFFFFFFF for i in self.data]
+
+    def AND(self, other):
+        self.data = [i & j for i, j in zip(self.data, other.data)]
+
+    def OR(self, other):
+        self.data = [i | j for i, j in zip(self.data, other.data)]
+
+    def XOR(self, other):
+        self.data = [i ^ j for i, j in zip(self.data, other.data)]
+
+    def shiftR(self, n):
+        self.data = [i >> n for i in self.data]
+
+    def shiftL(self, n):
+        self.data = [i << n for i in self.data]
+
+    def ADD(self, other):
+        carry = 0
+        for i in range(len(self.data)):
+            sum = self.data[i] + other.data[i] + carry
+            self.data[i] = sum & 0xFFFFFFFF
+            carry = sum >> 32
+        if carry != 0:
+            self.data.append(carry)
+
+    def SUB(self, other):
+        borrow = 0
+        for i in range(len(self.data)):
+            diff = self.data[i] - other.data[i] - borrow
+            if diff < 0:
+                diff += 1 << 32
+                borrow = 1
+            else:
+                borrow = 0
+            self.data[i] = diff
+
+def run_operation(bn1, bn2, hex1, hex2, operation, op_name):
+    bn1.setFromHexString(hex1)
+    bn2.setFromHexString(hex2)
+    if operation == bn1.INV:
+        operation()
+    else:
+        operation(bn2)
+    print(f'{op_name} = {bn1.getAsHexString()}\n')
+
+
+def run_tests():
+    bn1 = BigNumber()
+    bn2 = BigNumber()
+
+    run_operation(bn1, bn2, "51bf608414ad5726a3c1bec098f77b1b54ffb2787f8d528a74c1d7fde6470ea4",
+                  "403db8ad88a3932a0b7e8189aed9eeffb8121dfac05c3512fdb396dd73f6331c", bn1.INV, 'INV')
+    run_operation(bn1, bn2, "51bf608414ad5726a3c1bec098f77b1b54ffb2787f8d528a74c1d7fde6470ea4",
+                  "403db8ad88a3932a0b7e8189aed9eeffb8121dfac05c3512fdb396dd73f6331c", bn1.AND, 'AND')
+    run_operation(bn1, bn2, "51bf608414ad5726a3c1bec098f77b1b54ffb2787f8d528a74c1d7fde6470ea4",
+                  "403db8ad88a3932a0b7e8189aed9eeffb8121dfac05c3512fdb396dd73f6331c", bn1.OR, 'OR')
+    run_operation(bn1, bn2, "51bf608414ad5726a3c1bec098f77b1b54ffb2787f8d528a74c1d7fde6470ea4",
+                  "403db8ad88a3932a0b7e8189aed9eeffb8121dfac05c3512fdb396dd73f6331c", bn1.XOR, 'XOR')
+    run_operation(bn1, bn2, "36f028580bb02cc8272a9a020f4200e346e276ae664e45ee80745574e2f5ab80",
+                  "70983d692f648185febe6d6fa607630ae68649f7e6fc45b94680096c06e4fadb", lambda x: bn1.shiftL(5), 'ShiftL')
+    run_operation(bn1, bn2, "51bf608414ad5726a3c1bec098f77b1b54ffb2787f8d528a74c1d7fde6470ea4",
+                  "403db8ad88a3932a0b7e8189aed9eeffb8121dfac05c3512fdb396dd73f6331c", lambda x: bn1.shiftR(5), 'ShiftR')
+    run_operation(bn1, bn2, "36f028580bb02cc8272a9a020f4200e346e276ae664e45ee80745574e2f5ab80",
+                  "70983d692f648185febe6d6fa607630ae68649f7e6fc45b94680096c06e4fadb", bn1.ADD, 'ADD')
+    run_operation(bn1, bn2, "33ced2c76b26cae94e162c4c0d2c0ff7c13094b0185a3c122e732d5ba77efebc",
+                  "22e962951cb6cd2ce279ab0e2095825c141d48ef3ca9dabf253e38760b57fe03", bn1.SUB, 'SUB')
+
+def main():
+    run_tests()
+
+if __name__ == '__main__':
+    main()
+
