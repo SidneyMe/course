80f3097683695239c8ba469e2380b5795eadc2bd
SidneyMe <147240957+SidneyMe@users.noreply.github.com>
2023-11-01 22:57:42 +0200

    logic for lab_4 added

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-import lab_1 , lab_2 , lab_3
+import lab_1 , lab_2 , lab_3, lab_4
 
 def main():
     labs = {
         'lab_1': lab_1.main,
         'lab_2': lab_2.main,
         'lab_3': lab_3.main,
+        'lab_4': lab_4.main,
         
     }
 
     #labs['lab_1']()
     #labs['lab_2']()
-    labs['lab_3']()
+    #labs['lab_3']()
+    labs['lab_4']()
 
 if __name__ == '__main__':
     main()
