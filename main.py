94acc9a8f79199abd41e0f8c0dde11b658917ab0
SidneyMe <147240957+SidneyMe@users.noreply.github.com>
2023-11-22 22:52:49 +0200

    requirements added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import lab_1 , lab_2 , lab_3, lab_4, lab_5
+import lab_1 , lab_2 , lab_3, lab_4, lab_5, lab_7
 
 def main():
     labs = {
@@ -6,7 +6,8 @@ def main():
         'lab_2': lab_2.main,
         'lab_3': lab_3.main,
         'lab_4': lab_4.main,
-        'lab_4': lab_5.main,
+        'lab_5': lab_5.main,
+        'lab_7': lab_7.main,
         
     }
 
@@ -14,7 +15,9 @@ def main():
     #labs['lab_2']()
     #labs['lab_3']()
     #labs['lab_4']()
-    labs['lab_5']()
+    #labs['lab_5']()
+    labs['lab_7']()
+    
 
 if __name__ == '__main__':
     main()
